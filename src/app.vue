ad27a20cd9013fb31b91b390b38fe30ad74e00fa
Vladimir <u202022365@upc.edu.pe>
2024-04-23 21:02:34 -0500

    feat(created-users): added content for main view

--- src/app.vue
+++ src/app.vue
@@ -1,8 +1,23 @@
 <script>
+import {Vehicle} from './components/Vehicle';
+import vehicleListComponent from "./registration-management/components/vehicle-list.component.vue";
 
+export default {
+  name:'App',
+  components:{
+    vehicleListComponent
+  },
+  data(){
+    return{
+      vehicles:[]
+    }
+  },
+}
 </script>
 
 <template>
+
+
 
 </template>
 
