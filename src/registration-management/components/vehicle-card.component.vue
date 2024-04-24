2191fa3f3ca460c0211a213635c4d98ce56e52cb
Vladimir <u202022365@upc.edu.pe>
2024-04-23 20:37:45 -0500

    feat(created-users): add vehicle card for component

---
+++ src/registration-management/components/vehicle-card.component.vue
@@ -0,0 +1,22 @@
+<script>
+import {Vehicle} from "../model/vehicle.entity.js";
+export default {
+  name: "vehicle-card",
+  props:{
+    vehicle:{
+      type: Vehicle,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+
+
+
+</template>
+
+<style scoped>
+
+</style>
