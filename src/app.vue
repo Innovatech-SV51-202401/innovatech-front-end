8b2227fe6607b078f43507878282b86d00ca9636
QuiqueVladimir <U202022365@upc.edu.pe>
2024-04-29 01:55:11 -0500

    feat(registration-management): visualization vehicle component

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,8 @@
 <script>
 import vehicleListComponent from "./registration-management/components/vehicle-list.component.vue";
+import {Vehicle} from "./registration-management/model/vehicle.entity.js";
 import navbar from "./registration-management/components/navbar.component.vue";
+import vehiclesData from '../server/vehicles.json'
 
 export default {
   name:'App',
@@ -13,13 +15,20 @@ export default {
       vehicles:[]
     }
   },
-}
+  created(){
+    this.loadVehicles();
+  },
+  methods: {
+    loadVehicles(){
+      this.vehicles = vehiclesData.vehicles;
+  }
+}}
 </script>
 
 <template>
 
 <navbar class="navbar" />
-
+<vehicle-list-component :vehicles="vehicles"/>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './app.vue';
 import PrimeVue from "primevue/config";
+import 'primeflex/primeflex.css';
+import 'primevue/resources/themes/saga-blue/theme.css';
+
 
 import 'primevue/resources/themes/aura-light-green/theme.css';
 import 'primevue/resources/primevue.min.css'
@@ -9,6 +12,7 @@ import 'primeicons/primeicons.css'
 
 import Menubar from "primevue/menubar";
 import Image from "primevue/image";
+import Card from "primevue/card";
 
 
 
@@ -17,5 +21,5 @@ const app = createApp(App);
 app.use(PrimeVue,{ripple: true})
     .component('Menubar',Menubar)
     .component('Image', Image)
-
+    .component('pv-card', Card),
 app.mount('#app');
