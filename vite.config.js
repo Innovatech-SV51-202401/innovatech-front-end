62c658fc8071e82942f85fe75ea79a9b4a5b7557
QuiqueVladimir <U202022365@upc.edu.pe>
2024-04-30 23:34:37 -0500

    fix: update

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "innovatech-front-end",
   plugins: [vue()],
+  base: "https://Innovatech-SV51-202401.github.io/innovatech-front-end/",
   resolve: {
     alias: [
 
