fa0763cf1374c3791f8cf0cc3ab8bd7bf80e22cf
QuiqueVladimir <U202022365@upc.edu.pe>
2024-06-05 11:24:02 -0500

    feat(DaA): added client view component

---
+++ src/public/pages/profile/client-view.component.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  name: 'client-view',
+  data(){
+    return{
+      drawer: false,
+      items:[
+        {label: 'Home', to: '/client/home'},
+        {label: 'History', to: '/client/history'},
+        {label: 'Service', to : '/client/service'},
+        {label: 'Plans', to : '/client/plans'},
+        {label: 'Profile', to: '/client/profile'},
+        {label: 'Sign out', to: 'sign'}
+      ]
+    }
+  },
+  methods: {
+    toggleDrawer() {
+      this.drawer = !this.drawer;
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <pv-toolbar class="bg-primary" fixed>
+      <template #start>
+        <img src="https://i.ibb.co/rFt5BnP/file.png" alt="file" height="60px" width="auto">
+        <p class="text-lg w-10">GlideGo</p>
+      </template>
+      <template #end>
+        <div class="flex-column">
+          <router-link v-for="item in items" :key="item.label" v-slot="{navigate, href}" :to="item.to" custom>
+            <pv-button :href="href" class="p-button-text text-white" @click="navigate">{{item.label}}</pv-button>
+          </router-link>
+        </div>
+      </template>
+    </pv-toolbar>
+  </header>
+  <pv-sidebar v-model:visible="drawer"/>
+</template>
