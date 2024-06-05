740eba31a7afec61c2b0209f43da1f7ca633d5dd
QuiqueVladimir <U202022365@upc.edu.pe>
2024-06-05 11:24:36 -0500

    fix(DaA): correction index for added client component in home

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ import OwnerVehicle from "../service-execution-and-monitor/pages/owner/owner-veh
 import OwnerWallet from "../suscriptions-and-payments/pages/owner/owner-wallet.component.vue";
 import MechanicProfileComponent from "../profile-management/component/mechanic/mechanic-profile.component.vue";
 
-
+import ClientHomeComponent from "../dashboard-and-analytics/pages/client/client-home.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -24,9 +24,11 @@ const router = createRouter({
         {path: '/owner/history', component: MechanicHistory},
 
 
-        {path: '/client/home', component: MechanicHome},
+        {path: '/client/home', component: ClientHomeComponent},
         {path: '/client/history', component: MechanicHistory},
-        {path: '/client/reserve', component: MechanicHistory},
+        {path: '/client/service', component: MechanicHistory},
+        {path: '/client/plans', component: MechanicHistory},
+        {path: '/client/profile', component: MechanicHistory},
 
         {path: '/', redirect: '/home'},
 
