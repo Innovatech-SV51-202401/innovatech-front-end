b4789409b8bbe1cfdcefc8e680af038c68ea9c34
Sebastian <u201611430@upc.edu.pe>
2024-06-05 01:12:19 -0500

    feat(SEM): add reserved vehicle card component

---
+++ src/service-execution-and-monitor/components/client/reserved-vehicle-card.component.vue
@@ -0,0 +1,50 @@
+<template>
+  <pv-card class="p-3 reserved-vehicle-card">
+    <template #title>{{ vehicle.name }}</template>
+    <template #subtitle>Detalles de la Reserva</template>
+    <template #content>
+      <p>Fecha: {{ formattedDate }}</p>
+      <p>Hora: {{ formattedTime }}</p>
+    </template>
+  </pv-card>
+</template>
+
+<script>
+
+
+export default {
+  name: 'ReservedVehicleCard',
+  components: {
+
+
+  },
+  props: {
+    vehicle: {
+      type: Object,
+      required: true
+    },
+    reservationDate: {
+      type: Date,
+      required: true
+    },
+    reservationTime: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    formattedDate() {
+      return this.reservationDate.toDateString();
+    },
+    formattedTime() {
+      return this.reservationTime.label;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.reserved-vehicle-card {
+  margin-bottom: 16px;
+}
+</style>
