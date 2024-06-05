88dea5bc50d6e189bfd52668d2e9c971a2367afe
GamerCodeHL <yair360ThePro@gmail.com>
2024-06-04 22:51:38 -0500

    fix: modify routes and owner view component.

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,13 @@
 import {createRouter, createWebHistory} from "vue-router";
 
 import HomeMechanic from "../profile-management/component/mechanic/mechanic-home.component.vue";
-
 import MechanicProfile from "../profile-management/component/mechanic/mechanic-profile.component.vue";
 import MechanicWallet from "../profile-management/component/mechanic/mechanic-wallet.component.vue";
 import MechanicHistory from "../profile-management/component/mechanic/mechanic-history.component.vue";
+
+import OwnerHome from "../profile-management/component/owner/owner-home.component.vue";
+import OwnerWallet from "../glidego/components/owner/owner-wallet.component.vue";
+
 
 const router = createRouter({
     history: createWebHistory(),
@@ -13,7 +16,10 @@ const router = createRouter({
         {path: '/profile', component: MechanicProfile},
         {path: '/wallet', component: MechanicWallet},
         {path: '/history', component: MechanicHistory},
-        {path: '/', redirect: '/home'}
+        {path: '/', redirect: '/home'},
+        {path: '/owner/home', component: OwnerHome},
+        {path: '/owner/wallet', component: OwnerWallet},
+
     ]
 });
 
