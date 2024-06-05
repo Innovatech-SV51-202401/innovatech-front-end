438822592f7fc31085ac10026bd7a7272451ef7d
QuiqueVladimir <U202022365@upc.edu.pe>
2024-06-05 03:00:22 -0500

    feat(): fix app vue

--- src/public/pages/client/home-client.component.vue
+++ src/public/pages/client/home-client.component.vue
@@ -1,3 +1,26 @@
+<script>
+import ActiveServiceCardComponent from "../../glidego/components/client/active-service-card.component.vue";
+import ServiceHistoryComponent from "../../glidego/components/client/service-history.component.vue";
+export default {
+  name: 'clientView',
+  data(){
+    return{
+      drawer: false,
+      items:[
+        {label: 'Home', to: '/home'},
+        {label: 'History', to: '/history'},
+        {label: 'Wallet', to : '/wallet'},
+        {label: 'Profile', to: '/profile'},
+        {label: 'Sign out', to: 'sign'}
+      ]
+    }
+  },
+  components: {
+    ActiveServiceCardComponent,
+    ServiceHistoryComponent
+  }
+};
+</script>
 <template>
   <div class="grid nested-grid">
     <div class="col-12 md:col-6">
@@ -18,17 +41,6 @@
     </div>
   </div>
 </template>
-
-<script>
-import ActiveServiceCardComponent from "../../glidego/components/client/active-service-card.component.vue";
-import ServiceHistoryComponent from "../../glidego/components/client/service-history.component.vue";
-export default {
-  components: {
-    ActiveServiceCardComponent,
-    ServiceHistoryComponent
-  }
-};
-</script>
 
 <style scoped>
 /* Estilos si son necesarios */
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,33 @@
 import {createRouter, createWebHistory} from "vue-router";
-import HomeMechanic from "../profile-management/component/mechanic/mechanic-home.component.vue";
-import MechanicProfile from "../profile-management/component/mechanic/mechanic-profile.component.vue";
+import MechanicHome from "../dashboard-and-analytics/pages/mechanic/mechanic-home.component.vue";
 import MechanicWallet from "../suscriptions-and-payments/pages/mechanic/mechanic-wallet.component.vue";
 import MechanicHistory from "../profile-management/component/mechanic/mechanic-history.component.vue";
 
 import OwnerHome from "../profile-management/component/owner/owner-home.component.vue";
 import OwnerWallet from "../suscriptions-and-payments/pages/owner/owner-wallet.component.vue";
+import MechanicProfileComponent from "../profile-management/component/mechanic/mechanic-profile.component.vue";
+
+
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        {path: '/home', component: HomeMechanic},
-        {path: '/profile', component: MechanicProfile},
-        {path: '/wallet', component: MechanicWallet},
-        {path: '/history', component: MechanicHistory},
-        {path: '/', redirect: '/home'},
+        {path: '/mechanic/home', component: MechanicHome},
+        {path: '/mechanic/wallet', component: MechanicWallet},
+        {path: '/mechanic/history', component: MechanicHistory},
+        {path: '/mechanic/profile', component: MechanicProfileComponent},
+
         {path: '/owner/home', component: OwnerHome},
         {path: '/owner/wallet', component: OwnerWallet},
+        {path: '/owner/history', component: MechanicHistory},
+
+
+        {path: '/client/home', component: MechanicHome},
+        {path: '/client/history', component: MechanicHistory},
+        {path: '/client/reserve', component: MechanicHistory},
+
+        {path: '/', redirect: '/home'},
+
 
     ]
 });
